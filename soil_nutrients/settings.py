1834ae8f704be887e544b00c1aa59a28ead437eb
akibzaman <akibzaman19@gmail.com>
2021-10-20 23:46:38 +0600

    updated settings

--- soil_nutrients/settings.py
+++ soil_nutrients/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'soil_nutrients.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        os.path.join(BASE_DIR, 'db.sqlite3'),      
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
